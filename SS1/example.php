6adf97259eae741f15130b3b33aae435918357cd
MinhThong <pmthong303@gmail.com>
2017-09-28 19:47:40 +0700

    exampleOne

---
+++ SS1/example.php
@@ -0,0 +1,19 @@
+<html>
+   
+   <head>
+      <title>Ví dụ chương trình PHP</title>
+   </head>
+   <body>
+       <?php
+       	$x = 2;
+       	$y = 3;
+       	$z = $x + $y;
+       		echo "$z <br>";
+
+         echo "Bài học PHP đầu tiên <br>";
+         echo strlen("Minh Thông <br>"); 
+   
+        
+       ?>
+   </body>
+</html>
---
+++ SS1/test.php
@@ -0,0 +1,38 @@
+<html>
+   
+   <head>
+      <title>Ví dụ chương trình PHP</title>
+   </head>
+   <body>
+   		
+       <?php
+       	$x = 10;
+       	$y = 2;
+       	$z = $x + $y;
+       	$t = $x - $y;
+       	$s =$x * $y ;
+       	$d = $x / $y ;
+       		
+       		//echo $z."<br>" ;	// add
+       		//echo $t."<br>" ;	// sub
+       		//echo $s."<br>" ;	// mul
+       		//echo $d."<br>" ; 	//division
+       	function math ($x,$y){
+       		echo "$x * $y";
+       		echo "$x / $y";
+       	}
+       		
+
+       
+
+       
+         echo "Bài học PHP đầu tiên <br>";
+         echo strlen("Minh Thông <br>"); 
+
+         
+         
+   		
+       ?>
+
+   </body>
+</html>
